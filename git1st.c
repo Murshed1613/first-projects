c8e16220124345b1c83990c4161b9b7be4efbd54
Murshed1613 <murshedulhasan16@gmail.com>
2026-01-29 19:13:51 +0600

    code-changed

--- git1st.c
+++ git1st.c
@@ -1,6 +1,14 @@
-#include<stdio.h>
-int main(){
-    printf("Hello World!");
-    printf("its me murshed");
-    printf("I am frome teknaf.");
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    sum = a + b;
+
+    printf("Sum = %d", sum);
+    return 0;
 }
+
